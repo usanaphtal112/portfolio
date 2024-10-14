70fc5c0f4b1d8860ca6f73f1fc143c8c5b66b4eb
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-10-14 08:50:13 +0530

    fixed templates issue for navigation

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*", ".vercel.app", ".naphtal.tech"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"]
 
 
 # Application definition
