97e70fc06a6e76515411624a17af6162e4ed87fb
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2025-06-07 18:33:11 +0530

    Modified project and resume

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,20 +9,20 @@ import json
 
 class HomePageView(TemplateView):
     template_name = "components/home.html"
-    json_file_path = "projects.json"  # Adjust path as per your project structure
+    json_file_path = "projects.json"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         with open(self.json_file_path, "r") as f:
             projects_data = json.load(f)
         # Limiting projects to display on the home page
-        context["projectsData"] = projects_data[:2]  # Displaying first two projects
+        context["projectsData"] = projects_data[:2]
         return context
 
 
 class ProjectListView(TemplateView):
     template_name = "pages/project.html"
-    json_file_path = "projects.json"  # Adjust path as per your project structure
+    json_file_path = "projects.json"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
